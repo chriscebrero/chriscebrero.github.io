b4e46fd20255f5e97f7edcfc4819ed103caf07ed
christiancebrero <chris.cebrero@gmail.com>
2019-07-29 14:57:25 -0700

    Updates

---
+++ precache-manifest.b489f4662160261c0e39396d45340596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ac71e68630c0e27cc2be80afbb3b42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d195300bfc4ce35a0d57",
+    "url": "/static/css/main.bf961868.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d195300bfc4ce35a0d57",
+    "url": "/static/js/main.b8162f47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
